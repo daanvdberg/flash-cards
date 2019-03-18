4b6db87f7308e22a7e6dd5051c3d8239224c4bc1
Daan <daan.van.den.berg@tmogroup.asia>
2019-03-18 22:23:03 +0800

    CHANGE: Combine store, containers and navigation for entry

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StatusBar, View, StyleSheet } from 'react-native';
+import { Constants, Font } from 'expo';
+import configureStore from './redux/store';
+import AppContainer from './AppNavigator';
+import { Provider } from 'react-redux';
+import { receiveDecks } from './redux/actions/decks';
+import { getDecks } from './utils/_DATA';
+import getTheme from './native-base-theme/components';
+import { StyleProvider } from 'native-base';
+import flashCards from './native-base-theme/variables/flashCards';
+import { Notifications } from 'expo';
+import { setLocalNotification } from './utils/helpers'
 
-export default class App extends React.Component {
+const store = configureStore();
+
+function AppStatusBar ({ backgroundColor, ...props }) {
+	return (
+		<View style={ { backgroundColor, height: Constants.statusBarHeight } }>
+			<StatusBar
+				translucent
+				backgroundColor={ backgroundColor }
+				{ ...props }
+			/>
+		</View>
+	);
+}
+
+export default class App extends Component {
+
+	async componentDidMount() {
+		await Font.loadAsync({
+			'Roboto': require('native-base/Fonts/Roboto.ttf'),
+			'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+		});
+		getDecks().then((res) => store.dispatch(receiveDecks(res)));
+
+		setLocalNotification();
+	}
+
 	render () {
 		return (
-			<View style={ styles.container }>
-				<Text>Open up App.js to start working on your app!</Text>
-			</View>
+			<Provider store={ store }>
+				<StyleProvider  style={getTheme(flashCards)}>
+					<View style={ { flex: 1 } }>
+						<AppStatusBar backgroundColor='teal' barStyle='light-content' />
+						<AppContainer />
+					</View>
+				</StyleProvider>
+			</Provider>
 		);
 	}
 }
 
 const styles = StyleSheet.create( {
 	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+		backgroundColor: 'hsl(0, 0%, 97%)'
+	}
 } );
